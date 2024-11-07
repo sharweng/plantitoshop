59757d2be3a5a1b1bbcddc73ee8bfd6f3c5e0f2b
sharweng <marbellasharwinjohn@gmail.com>
2024-11-07 22:39:48 +0800

    bootstrap link

--- includes/header.php
+++ includes/header.php
@@ -7,48 +7,7 @@
 ?>
 <link rel="stylesheet" href="styles/style.css">
 <style type="text/css">
-    body {
-        font-family: Helvetica;
-        margin: 0;
-    }
-    a {
-        text-decoration: none;
-        color: #000;
-    }
-    .site-header { 
-        background-color: white;
-        position: fixed;
-        top: 0;
-        right: 0;
-        left: 0;
-        border-bottom: 1px solid #ccc;
-        padding: .5em 1em;
-        display: flex;
-        justify-content: space-between;
-        z-index: 10;
-    }
-
-    .site-identity h1 {
-        font-size: 1.5em;
-        margin: .6em 0;
-        display: inline-block;
-    }
-
-    .site-navigation ul, 
-    .site-navigation li {
-        margin: 0; 
-        padding: 0;
-    }
-
-    .site-navigation li {
-        display: inline-block;
-        margin: 1.4em 1em 1em 1em;
-    }
-
-    .green-hover:hover{
-        cursor: pointer;
-        color: #337357;
-    }
+    
 </style>
 <header class="site-header">
     <div class="site-identity">
--- product/index.php
+++ product/index.php
@@ -10,7 +10,7 @@
         $keyword = strtolower(trim($_GET['search']));
     else
         $keyword = "";
-    
+
     if($keyword){
         $sql = "SELECT p.prod_id, p.description, p.price, s.quantity FROM product p INNER JOIN stock s ON p.prod_id = s.prod_id WHERE description LIKE '%{$keyword}%'";
         $result = mysqli_query($conn, $sql);
@@ -25,6 +25,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         <?php include('../includes/styles/style.css') ?>
         form{
