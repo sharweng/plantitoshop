12c4f80b425acdde8d44bfee3c60c7c445654319
sharweng <marbellasharwinjohn@gmail.com>
2024-11-07 23:54:32 +0800

    thinkering with bootstrap

--- product/index.php
+++ product/index.php
@@ -33,64 +33,87 @@
             margin: 0;
             padding: 0;
         }
+        .container{
+            border: 1px solid red;
+        }
     </style>
 </head>
 <body>
     <h1>This is the product page.</h1>
-    <a href="create.php">
-        <button class="add-button">ADD</button>
-    </a>
-    <form action="" method="get">
-        <input type="text" name="search">
-        <button>Search</button>
-    </form>
     <div class="container">
-    <?php 
-        while($row = mysqli_fetch_array($result)){
-            echo "
-                <table>
-                    <tr class=\"details\">
-                        <td>{$row['prod_id']}</td>
-                        <td>";
-            $sql = "SELECT img_path FROM image WHERE prod_id = {$row['prod_id']}";
-            $result2 = mysqli_query($conn, $sql);
-            while($row2 = mysqli_fetch_array($result2)){
-                echo "<img src=\"{$row2['img_path']}\" height=\"100px\" width=\"100px\">";
+        <div class="row top-header">
+            <div class="col-4 d-flex align-items-center justify-content-start">
+                <a href="create.php">
+                    <button class="btn btn-success btn-sm">ADD</button>
+                </a>
+            </div>
+            <div class="col-8 d-flex align-items-center justify-content-end">
+            <form action="" method="get">
+                <div class="row d-grid gap-1">
+                    <div class="d-block">
+                        <input type="text" name="search">
+                    </div>
+                    <div class="d-block">
+                        <button class="btn btn-success btn-sm w-100">Search</button>
+                    </div>
+                    
+                    
+                </div>
+                
+            </form>
+            </div>
+        </div>
+        
+        
+        <div class="container">
+        <?php 
+            while($row = mysqli_fetch_array($result)){
+                echo "
+                    <table>
+                        <tr class=\"details\">
+                            <td>{$row['prod_id']}</td>
+                            <td>";
+                $sql = "SELECT img_path FROM image WHERE prod_id = {$row['prod_id']}";
+                $result2 = mysqli_query($conn, $sql);
+                while($row2 = mysqli_fetch_array($result2)){
+                    echo "<img src=\"{$row2['img_path']}\" height=\"100px\" width=\"100px\">";
+                }
+                            echo "</td>
+                            <td>{$row['description']}</td>
+                            <td>{$row['price']}</td>
+                            <td>{$row['quantity']}</td>
+                            <td>
+                                <form action=\"\" method=\"post\">
+                                    <button name=\"update_btn\" value=\"{$row['prod_id']}\">EDIT</button>
+                                </form>";
+                if(isset($_POST['update_btn'])&&!isset($_POST['no'])&&$_POST['update_btn']==$row['prod_id']){
+                    echo "<form action=\"edit.php\" method=\"post\">
+                        <button name=\"update_id\" value=\"{$row['prod_id']}\">YES</button>
+                    </form>
+                    <form action=\"\" method=\"post\">
+                        <button name=\"no\" value=\"{$row['prod_id']}\">NO</button>
+                    </form>";
+                }
+                                echo"<form action=\"\" method=\"post\">
+                                    <button name=\"delete_btn\" value=\"{$row['prod_id']}\">DELETE</button>
+                                </form>";
+                if(isset($_POST['delete_btn'])&&!isset($_POST['no'])&&$_POST['delete_btn']==$row['prod_id']){
+                    echo "<form action=\"delete.php\" method=\"post\">
+                        <button name=\"delete_id\" value=\"{$row['prod_id']}\">YES</button>
+                    </form>
+                    <form action=\"\" method=\"post\">
+                        <button name=\"no\" value=\"{$row['prod_id']}\">NO</button>
+                    </form>";
+                }
+                echo"</td>
+                        </tr>
+                    </table>
+                ";
             }
-                        echo "</td>
-                        <td>{$row['description']}</td>
-                        <td>{$row['price']}</td>
-                        <td>{$row['quantity']}</td>
-                        <td>
-                            <form action=\"\" method=\"post\">
-                                <button name=\"update_btn\" value=\"{$row['prod_id']}\">EDIT</button>
-                            </form>";
-            if(isset($_POST['update_btn'])&&!isset($_POST['no'])&&$_POST['update_btn']==$row['prod_id']){
-                echo "<form action=\"edit.php\" method=\"post\">
-                    <button name=\"update_id\" value=\"{$row['prod_id']}\">YES</button>
-                </form>
-                <form action=\"\" method=\"post\">
-                    <button name=\"no\" value=\"{$row['prod_id']}\">NO</button>
-                </form>";
-            }
-                            echo"<form action=\"\" method=\"post\">
-                                <button name=\"delete_btn\" value=\"{$row['prod_id']}\">DELETE</button>
-                            </form>";
-            if(isset($_POST['delete_btn'])&&!isset($_POST['no'])&&$_POST['delete_btn']==$row['prod_id']){
-                echo "<form action=\"delete.php\" method=\"post\">
-                    <button name=\"delete_id\" value=\"{$row['prod_id']}\">YES</button>
-                </form>
-                <form action=\"\" method=\"post\">
-                    <button name=\"no\" value=\"{$row['prod_id']}\">NO</button>
-                </form>";
-            }
-            echo"</td>
-                    </tr>
-                </table>
-            ";
-        }
-    ?>
+        ?>
+        </div>
     </div>
+    
 </body>
 </html>
 <?php
