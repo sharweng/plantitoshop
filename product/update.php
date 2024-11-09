a95cda76b6f9b16a831ad38f8fb9b2d6491b507f
sharweng <marbellasharwinjohn@gmail.com>
2024-11-10 00:56:17 +0800

    update changes (del not working)

--- product/update.php
+++ product/update.php
@@ -40,44 +40,105 @@
             
             
             if(isset($_FILES['img_path'])){
+                $sql = "SELECT img_id, img_path FROM image WHERE prod_id = {$u_id}";
+                $result = mysqli_query($conn, $sql);
+                $existingImages = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $existingCount = count($existingImages);
+                $uploadedFiles = $_FILES['img_path']['name'];
+                $uploadCount = count($uploadedFiles);
+
+                echo $uploadCount . " " . $existingCount . "<br>";;
+                $counter = 0;
+                foreach ($uploadedFiles as $key => $name) {
+                    $source = $_FILES['img_path']['tmp_name'][$key];
+                    $target = 'images/' . basename($name);
+
+                    if (move_uploaded_file($source, $target)) {
+                        if ($counter < $existingCount) {
+                            $img_sql = "UPDATE image SET img_path = '{$target}' WHERE img_id = {$existingImages[$counter]['img_id']}";
+                        } elseif ($counter < $uploadCount) {
+                            $img_sql = "INSERT INTO image (prod_id, img_path) VALUES ({$u_id}, '{$target}')";
+                        } else {
+                            $img_sql = "DELETE FROM image WHERE img_id = {$existingImages[$counter]['img_id']}";
+                        }
+
+                        echo $img_sql . "<br>";
+                        $result3 = mysqli_query($conn, $img_sql);
+
+                        if (!$result3) {
+                            $isSuccess = false;
+                        }
+                    }
+                    $counter++;
+                    echo $counter . "<br>";
+                }
+                /*
                 $sql = "SELECT COUNT(img_path) as count FROM image WHERE prod_id = {$u_id}";
-                echo $sql;
                 $count_query = mysqli_query($conn, $sql);
                 $numOfImg = mysqli_fetch_array($count_query);
                 $count = $numOfImg['count'];
-                
 
+                $numFiles = count($_FILES['img_path']['name']);
+                echo $numFiles . " " . $count . "<br>";
                 $isSuccess = true;
                 $counter = 0;
                 foreach($_FILES['img_path']['name'] as $key => $name){
                     $source = $_FILES['img_path']['tmp_name'][$key];
                     $target = 'images/' . basename($name);
 
+                    $sql = "SELECT * FROM image WHERE prod_id = {$u_id}";
+                    echo $sql . "<br>";
+                    $result4 = mysqli_query($conn, $sql);
                     if (move_uploaded_file($source, $target)) {
-                        if($counter > $count){
-                            $img_sql = "INSERT INTO image(prod_id, img_path) VALUES({$u_id}, '{$target}')";
-                            $result3 = mysqli_query($conn, $img_sql);
+                        if($numFiles < $count){
+                            while($row = mysqli_fetch_array($result4)){
+                                if($counter < $numFiles){
+                                    $img_sql = "UPDATE image SET img_path = '{$target}' WHERE img_id = {$row['img_id']}";
+                                    echo $img_sql . "<br>";
+                                    $result3 = mysqli_query($conn, $img_sql);
+                                }else{
+                                    $img_sql = "DELETE FROM image WHERE img_id = {$row['img_id']}";
+                                    echo $img_sql . "<br>";
+                                    $result3 = mysqli_query($conn, $img_sql);            
+                                }
+                                $counter++;
+                                $counter . "<br>";
+                            }    
+                        }elseif($numFiles > $counter){
+                            while($row = mysqli_fetch_array($result4)){
+                                if($counter < ($numFiles-$count)){
+                                    $img_sql = "INSERT INTO image(prod_id, img_path) VALUES({$u_id}, '{$target}')";
+                                    echo $img_sql . "<br>";
+                                    $result3 = mysqli_query($conn, $img_sql);
+                                }else{
+                                    $img_sql = "UPDATE image SET img_path = '{$target}' WHERE img_id = {$row['img_id']}";
+                                    echo $img_sql . "<br>";
+                                    $result3 = mysqli_query($conn, $img_sql);            
+                                }
+                                $counter++;
+                                $counter . "<br>";
+                            }  
                         }else{
-                            $sql = "SELECT * FROM image WHERE prod_id = {$u_id}";
-                            $result4 = mysqli_query($conn, $sql);
                             while($row = mysqli_fetch_array($result4)){
                                 $img_sql = "UPDATE image SET img_path = '{$target}' WHERE img_id = {$row['img_id']}";
+                                echo $img_sql . "<br>";
                                 $result3 = mysqli_query($conn, $img_sql);
-                            }             
+                            }     
                         }
+                        
                         if($isSuccess == false)
                             $isSuccess = false;
                         if(!$result3)
                             $isSuccess = false;     
                     }
-                    $counter++;
                 }
+                    */
             }
-
+            
             if($result && $result2) {
                 $_SESSION['desc'] = "";
                 $_SESSION['prc'] = "";
-                header("Location: index.php");
+                //header("Location: index.php");
             }
         }
         
