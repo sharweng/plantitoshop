3e03b0714851548dc3b4e97aeb56efb92e1f1c2b
sharweng <marbellasharwinjohn@gmail.com>
2024-11-09 20:54:43 +0800

    product index bootstrap

--- product/index.php
+++ product/index.php
@@ -33,68 +33,70 @@
             margin: 0;
             padding: 0;
         }
-        .container{
-            border: 1px solid red;
-        }
     </style>
 </head>
 <body>
-    <h1>This is the product page.</h1>
-    <div class="container">
-        <div class="row top-header">
-            <div class="col-4 d-flex align-items-center justify-content-start">
+    <h1 class="text-center p-2">This is the product page.</h1>
+    <div class="container-sm outer-box p-3 mb-3 shadow-lg  border border-success border-2">
+        <div class="row top-header pb-3 justify-content-between">
+            <div class="col-2 d-flex align-items-center justify-content-start">
                 <a href="create.php">
-                    <button class="btn btn-success btn-sm">ADD</button>
+                    <button class="btn btn-success">ADD</button>
                 </a>
             </div>
-            <div class="col-8 d-flex align-items-center justify-content-end">
-            <form action="" method="get">
-                <div class="row d-grid gap-1">
-                    <div class="d-block">
-                        <input type="text" name="search">
+            <div class="col-4 d-flex align-items-center justify-content-end">
+                <form action="" method="get">
+                    <div class="row d-grid gap-1">
+                        <div class="d-block">
+                            <input type="text" name="search">
+                        </div>
+                        <div class="d-block">
+                            <button class="btn btn-success w-100 btn-sm">Search</button>
+                        </div>
                     </div>
-                    <div class="d-block">
-                        <button class="btn btn-success btn-sm w-100">Search</button>
-                    </div>
-                </div>
-            </form>
+                </form>
             </div>
         </div>
-        <div class="container">
+        <div class="container inner-box border border-success border-2">
         <?php 
             while($row = mysqli_fetch_array($result)){
                 echo "
-                    <table class=\"table table-hover\">
-                        <tr class=\"details\">
-                            <td>{$row['prod_id']}</td>
-                            <td>";
+                    <table class=\"table text-center \">
+                        <tr class=\"details row d-grid\">
+                            <td class=\"col d-flex flex-wrap align-items-center justify-content-center\">";
                 $sql = "SELECT img_path FROM image WHERE prod_id = {$row['prod_id']}";
                 $result2 = mysqli_query($conn, $sql);
                 while($row2 = mysqli_fetch_array($result2)){
-                    echo "<img src=\"{$row2['img_path']}\" height=\"100px\" width=\"100px\">";
+                    echo "<img class=\"img-thumbnail\" src=\"{$row2['img_path']}\" height=\"100px\" width=\"100px\">";
                 }
                             echo "</td>
-                            <td>{$row['description']}</td>
-                            <td>{$row['price']}</td>
-                            <td>{$row['quantity']}</td>
-                            <td class=\"row d-grid gap-1\">
-                                <form action=\"edit.php\" method=\"post\">
-                                    <button class=\"btn btn-warning btn-sm w-100\" name=\"update_id\" value=\"{$row['prod_id']}\">EDIT</button>
-                                </form>
-                                <div class=\"dropdown dropend d-block\">
-                                    <button class=\"btn btn-danger btn-sm w-100 dropdown-toggle\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">
-                                        DELETE
-                                    </button>
-                                    <ul class=\"dropdown-menu\">
-                                        <form action=\"delete.php\" method=\"post\">
-                                            <button class=\"dropdown-item btn-sm w-100\" name=\"delete_id\" value=\"{$row['prod_id']}\">YES</button>
+                            <td class=\"col d-flex align-items-center\">
+                                <div class=\"col d-grid align-items-center\">{$row['prod_id']}</div>
+                                <div class=\"col d-grid align-items-center\">{$row['description']}</div>
+                                <div class=\"col d-grid align-items-center\">{$row['price']}</div>
+                                <div class=\"col d-grid align-items-center\">{$row['quantity']}</div>
+                                <div class=\"col d-grid align-items-center\">
+                                    <div class=\"row d-grid gap-1\">
+                                        <form action=\"edit.php\" method=\"post\">
+                                            <button class=\"btn btn-warning btn-sm w-100\" name=\"update_id\" value=\"{$row['prod_id']}\">EDIT</button>
                                         </form>
-                                        <form action=\"\" method=\"post\">
-                                            <button class=\"dropdown-item btn-sm w-100\" name=\"no\" value=\"{$row['prod_id']}\">NO</button>
-                                        </form>
-                                    </ul>
+                                        <div class=\"dropdown d-block\">
+                                            <button class=\"btn btn-danger btn-sm w-100 dropdown-toggle\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">
+                                                DELETE
+                                            </button>
+                                            <ul class=\"dropdown-menu\">
+                                                <form action=\"delete.php\" method=\"post\">
+                                                    <button class=\"dropdown-item btn-sm w-100\" name=\"delete_id\" value=\"{$row['prod_id']}\">YES</button>
+                                                </form>
+                                                <form action=\"\" method=\"post\">
+                                                    <button class=\"dropdown-item btn-sm w-100\" name=\"no\" value=\"{$row['prod_id']}\">NO</button>
+                                                </form>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </td>
+                            
                         </tr>
                     </table>
                 ";
