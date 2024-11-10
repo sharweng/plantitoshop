32e784c065022c9daa7801df6ea3472fb6ab17db
sharweng <marbellasharwinjohn@gmail.com>
2024-11-10 20:00:10 +0800

    rounded corners

--- product/create.php
+++ product/create.php
@@ -29,7 +29,7 @@
 </head>
 <body>
     <h1 class="text-center p-2 fw-bold">This is the add product page.</h1>
-    <div class="container-sm outer-box p-3 mb-3 shadow-lg  border border-success border-2">
+    <div class="container-sm outer-box p-3 mb-3 shadow-lg  border border-success border-2 rounded">
         <div class="row top-header pb-3 justify-content-between">
             <div class="col-2 d-flex align-items-center justify-content-start">
                 <form action="" method="post">
--- user/login.php
+++ user/login.php
@@ -47,7 +47,7 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
     <h1 class="text-center p-2 fw-bold">This is the login page.</h1>
-    <div class="container-sm outer-box p-3 mb-3 shadow-lg  border border-success border-2">
+    <div class="container-sm outer-box p-3 mb-3 shadow-lg  border border-success border-2 rounded">
         <div class="row top-header pb-3 justify-content-between">
             <div class="col-4 d-flex align-items-center justify-content-start">
                 <a href="/plantitoshop/">
